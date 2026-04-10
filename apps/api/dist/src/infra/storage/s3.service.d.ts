df9dc4bdfe18b1f10c171aa5e525f02e8fd9d2c9
Miguel Perdomo <miguelferneyp@gmail.com>
2026-04-10 17:11:23 -0500

    feat: dockerize monorepo and cleanup build artifacts from git

--- apps/api/dist/src/infra/storage/s3.service.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class S3Service {
-    private client;
-    constructor();
-    checkConnection(): Promise<boolean>;
-}
--- apps/web/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"/yexS3MRiP/qzXWgJbzVyU0q39+VV8DjFE1LOSrm73g=\"\n}"
